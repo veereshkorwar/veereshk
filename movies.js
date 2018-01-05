c8cb7e6b61690d0c1aa3160f38d76b622c1e60d5
veereshkorwar <32124177+veereshkorwar@users.noreply.github.com>
2018-01-05 16:11:03 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,22 @@
+module.exports ={
+  firstName :"veeresh",
+  lastName : "korwar",
+  favMovie: "DDLJ",
+  fullName :function(){
+    return this.firstName + this.lastName;
+  }
+  qualification :"BE(ECE)",
+  profession:"Software Engineer",
+  place : "Bangalore",
+  mobNo:"9538939082"
+
+};
+
+
+/*function ddlj(){
+console.log('DDLJ is my fav movie...');
+}
+
+//module.exports.movies=rush;
+module.exports.user = myself;
+*/
